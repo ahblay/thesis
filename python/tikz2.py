c8cdde601947333a0ca05daa2029e21c500fdebc
Abel Romer <abel.romer@questu.ca>
2022-01-27 16:55:58 -0800

    Some additional writing and figures.

--- python/tikz2.py
+++ python/tikz2.py
@@ -171,6 +171,62 @@ def heatmap(grids, scale, filename, chapter):
 
 
 # takes a numpy ndarray (representing a particular starting infection) as an argument
+# builds frames with numbered infections
+def numbered_heatmap(grids, scale, filename, chapter):
+    # create document
+    doc = Document(documentclass="standalone")
+
+    shape = grids[0].shape
+
+    # these may be wrong....
+    layers = shape[0]
+    rows = shape[1]
+    cols = shape[2]
+
+    counter = 0
+    to_draw = []
+    added = []
+
+    scale_args = {"scale": scale}
+    with doc.create(TikZ(options=TikZOptions(**scale_args))) as pic:
+        for grid in grids:
+            for l in range(layers):
+                for r in range(rows):
+                    for c in range(cols):
+                        if grid[l][r][c] == 1:
+                            if counter == 0:
+                                node_kwargs = {'fill': 'red',}
+                                circle = TikZDraw([TikZCoordinate(c, 0 - (r + (l * (rows + 1)))),
+                                                   'rectangle',
+                                                  TikZCoordinate(c+1, 1 - (r + (l * (rows + 1))))],
+                                                  options=TikZOptions('draw',
+                                                                      **node_kwargs))
+                            else:
+                                #node_kwargs = {'label': f'{counter}', }
+                                node = TikZNode(text='My block',
+                                                            handle='box',
+                                                            options=TikZOptions('draw',
+                                                                                'rounded corners'))
+                                circle = TikZDraw([TikZCoordinate(c, 0 - (r + (l * (rows + 1)))),
+                                                   'rectangle',
+                                                   node,
+                                                   TikZCoordinate(c + 1, 1 - (r + (l * (rows + 1))))],
+                                                  options=TikZOptions('draw',
+                                                                      **node_kwargs))
+                            if (c, 0 - (r + (l * (rows + 1)))) not in added:
+                                to_draw.append(circle)
+                                added.append((c, 0 - (r + (l * (rows + 1)))))
+                        else:
+                            pass
+            counter += 1
+        for item in to_draw:
+            pic.append(item)
+
+    doc.generate_pdf(f'/Users/abel/Documents/School/Grad School/Research?/Bootstrap Percolation/Thesis/figures/{chapter}/{filename}_numbered_heatmap', clean_tex=False)
+    return
+
+
+# takes a numpy ndarray (representing a particular starting infection) as an argument
 # builds frames in better format
 def build_frames(grids, scale, filename, chapter, gradient=False, circle_node=True):
 
@@ -257,26 +313,38 @@ def dialog():
 
     if step_amount == 'y':
         if h == 'y':
-            heatmap(steps, scale, filename, chapter)
+            numbered_heatmap(steps, scale, filename, chapter)
+            filename = filename + "_numbered_heatmap"
         else:
             build_frames(steps, scale, filename, chapter, circle_node=circles)
     else:
         step_amount = input('first step? [y/n]:')
         if step_amount == 'y':
             if h == 'y':
-                heatmap(steps[0:1], scale, filename, chapter)
+                numbered_heatmap(steps[0:1], scale, filename, chapter)
+                filename = filename + "_numbered_heatmap"
             else:
                 build_frames(steps[0:1], scale, filename, chapter, circle_node=circles)
         else:
             step_amount = input('first two steps? [y/n]:')
             if step_amount == 'y':
                 if h == 'y':
-                    heatmap(steps[0:2], scale, filename, chapter)
+                    numbered_heatmap(steps[0:2], scale, filename, chapter)
+                    filename = filename + "_numbered_heatmap"
                 else:
                     build_frames(steps[0:2], scale, filename, chapter, circle_node=circles)
             else:
-                print("ABORTING without diagram")
-                return
+                step_amount = input('first n steps? [enter a number, or n to abort]:')
+                if step_amount != 'n':
+                    step_amount = min(int(step_amount), len(steps))
+                    if h == 'y':
+                        numbered_heatmap(steps[0:step_amount], scale, filename, chapter)
+                        filename = filename + "_numbered_heatmap"
+                    else:
+                        build_frames(steps[0:step_amount], scale, filename, chapter, circle_node=circles)
+                else:
+                    print("ABORTING without diagram")
+                    return
     print(f"diagram saved to: figures/{chapter}/{filename}.pdf")
     pyperclip.copy(f"figures/{chapter}/{filename}.pdf")
     return
